fc97c24a83f733615614640ec665f81237753189
Oscar Membrilla Estorach <impactes@gmail.com>
2016-03-02 22:17:37 +0100

    Empezamos con esta base

---
+++ Prova/src/prova/BDHelper.java
@@ -0,0 +1,58 @@
+//Creat per Oscar Membrilla Estorach
+
+package prova;
+
+import java.sql.*;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
+
+/**
+ *
+ * @author Oscar
+ */
+public class BDHelper {
+    
+    Connection con = null;
+    Statement statement = null;
+    ResultSet rs = null;
+    
+    public Connection conectarDB(String ip, String port, String user, String pass) {
+        
+        try {
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://" + ip + ":" + port + "/dbchronoschedule", "standard", "1234");
+            
+        } catch(ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error en: " + e);
+        }
+        
+        return con;
+    }
+    
+    public boolean login(String user, String pass) {
+        
+        String sentenciaSQL = "SELECT * " + 
+                              "FROM users " +
+                              "WHERE userName = '" + user + 
+                                    "' AND pass = '" + pass + "';";
+        
+        try {
+            
+            statement = con.createStatement();
+            rs = statement.executeQuery(sentenciaSQL);
+            
+            if (rs.first()) {
+                return true;
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(BDHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+    
+}
