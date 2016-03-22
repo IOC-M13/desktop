9871a648afd166d4041780e47895d2a2123b0f07
Oscar Membrilla Estorach <impactes@gmail.com>
2016-03-22 21:54:23 +0100

    Crear, en comentarios de la clase 'FileProperties', un Properties.config que funcione bajo cualquier path

--- src/controller/FileProperties.java
+++ src/controller/FileProperties.java
@@ -9,9 +9,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.CodeSource;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -22,15 +26,38 @@ public class FileProperties {
     private Properties prop;
     private InputStream in;
     private OutputStream out;
+    private String jarDir;
     
     public FileProperties() {
         prop = new Properties();
+        
+        /*
+        
+        // For run with .jar
+        
+        CodeSource codeSource = FileProperties.class.getProtectionDomain().getCodeSource();
+        File jarFile = null;
+        try {
+            jarFile = new File(codeSource.getLocation().toURI().getPath());
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(FileProperties.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        jarDir = jarFile.getParentFile().getAbsolutePath();
+        
+        */
     }
     
     public void loadProperties() {
         
         try {
             in = new FileInputStream("src" + File.separator + "resources" + File.separator + "config.properties");
+            /**
+             * 
+             * For run with .jar
+             * 
+             * in = new FileInputStream(jarDir + File.separator + "config.properties");
+             * 
+             **/
             
             //Cargar el fichero de propiedades
             prop.load(in);
@@ -41,9 +68,9 @@ public class FileProperties {
             
             
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(FileProperties.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Error at read: " + ex.getMessage());
         } catch (IOException ex) {
-            Logger.getLogger(FileProperties.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Error at read: " + ex.getMessage());
         } finally {
             closeProperties();
         }
@@ -54,6 +81,14 @@ public class FileProperties {
         try {
             out = new FileOutputStream("src" + File.separator + "resources" + File.separator + "config.properties");
             
+            /**
+             * 
+             * For run with .jar
+             * 
+             * out = new FileOutputStream(jarDir + File.separator + "config.properties");
+             * 
+             **/
+            
             // Asignar los valores a las propiedades
             prop.setProperty("ip", Support.IP);
             prop.setProperty("port", Support.port);
@@ -63,9 +98,9 @@ public class FileProperties {
             
             
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(FileProperties.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Error at write: " + ex.getMessage());
         } catch (IOException ex) {
-            Logger.getLogger(FileProperties.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Error at write: " + ex.getMessage());
         } finally {
             closeProperties();
         }
@@ -73,7 +108,21 @@ public class FileProperties {
     
     
     public void closeProperties() {
+        if (in != null) {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         
+        if (out != null) {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
     
 
